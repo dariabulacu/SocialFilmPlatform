1e9583de016288e2a4d975483636c0cab343f6b9
dariaorsmth <dariabulacu@yahoo.com>
2025-12-15 21:57:39 +0200

    Added Views/Review and ReviewsController

--- SocialFilmPlatform/Controllers/Reviews.cs
+++ SocialFilmPlatform/Controllers/Reviews.cs
@@ -23,5 +23,35 @@ namespace SocialFilmPlatform.Controllers
                 return RedirectToAction("/Movies/Show/" + rev.MovieId);
             }
         }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Review rev = db.Reviews.Find(id);
+            db.Reviews.Remove(rev);
+            db.SaveChanges();
+            return RedirectToAction("/Movies/Show/" + rev.MovieId);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Review rev = db.Reviews.Find(id);
+            return View(rev);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit (int id, Review requestRev)
+        {
+            Review rev = db.Reviews.Find(id);
+            if (ModelState.IsValid)
+            {
+                rev.Content = requestRev.Content;
+                db.SaveChanges();
+                return Redirect("/Movies/Show/" + rev.MovieId);
+            }else
+            {
+                return View(requestRev);
+            }
+        }
     }
 }
